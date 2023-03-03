80ac47921d9b0260e90391c4e4432f730fb0b189
ItVal <valentinhonvj1@gmail.com>
2023-03-03 09:49:54 +0100

    feat post route : create session checkout

--- index.js
+++ index.js
@@ -11,9 +11,34 @@ app.use(express.json())
 app.use(cors());
 
 //routes
+//get
 app.get('/api/pay', (req, res) => {
     res.send("Hello world")
 });
+
+//post
+app.post('/api/pay/create', async (req, res) => {
+    const {product} = req.body;
+    const session = await stripe.checkout.sessions.create({
+        payment_method: ["card"],
+        line_items: [
+            {
+                price_data: {
+                    currency: "USD",
+                    product_data: {
+                        name: product.name,
+                    },
+                    unit_amount: product.price * 100,
+                },
+                quantity: product.quantity,
+            },
+        ],
+        mode: 'payment',
+        success_url: "http://localhost:300/success",
+        cancel_url: "http://localhost:300/cancel"
+    });
+    res.json({id: session.id});
+})
 
 //listen server
 app.listen(8900, () => {
