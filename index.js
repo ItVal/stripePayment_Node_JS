6d77bef810d741dd059ae73708387e65d339ebd6
ItVal <valentinhonvj1@gmail.com>
2023-03-04 18:15:36 +0100

    refact index to es6

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import cors from ('cors');
-import express from ('express');
-import {postStripePayment} from ('./routes/stripe.routes.js')
+import cors from 'cors';
+import express from 'express';
+import postStripePayment from './routes/stripe.routes.js';
 const app = express();
 
 //Middlewares 
--- routes/stripe.routes.js
+++ routes/stripe.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const route = express.Router();
-const { postStripePayment } = require('../controllers/stripe.controllers');
+const postStripePayment = require('../controllers/stripe.controllers');
 
 route.post('/', postStripePayment);
 
