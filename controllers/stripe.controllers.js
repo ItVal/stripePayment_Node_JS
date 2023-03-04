3283323e0b7ecb3ca5059b442f3d46aae42cf936
ItVal <valentinhonvj1@gmail.com>
2023-03-04 18:00:23 +0100

    feat create stripe controllers

---
+++ controllers/stripe.controllers.js
@@ -0,0 +1,31 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+require('dotenv').config();
+
+export const postStripePayment = async (req, res, next) => {
+	try {
+        const {product} = req.body;
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            line_items: [
+                {
+                    price_data: {
+                        currency: "USD",
+                        product_data: {
+                            name: product.name,
+                        },
+                        unit_amount: product.price * 100,
+                    },
+                    quantity: product.quantity,
+                },
+            ],
+            mode: 'payment',
+            success_url: "http://localhost:5173/success",
+            cancel_url: "http://localhost:5173/cancel"
+        });
+        res.status(201).json({id: session.id});
+	} catch (error) {
+		const err = new Error('Internal error');
+		res.statusCode = 500;
+		return next(error);
+	}
+};
--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const cors = require('cors');
 const express = require('express');
-require('dotenv').config();
-
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
@@ -15,30 +12,8 @@ app.use(cors());
 app.get('/api/pay', (req, res) => {
     res.send("Hello world")
 });
-
 //post
-app.post('/api/pay/create', async (req, res) => {
-    const {product} = req.body;
-    const session = await stripe.checkout.sessions.create({
-        payment_method: ["card"],
-        line_items: [
-            {
-                price_data: {
-                    currency: "USD",
-                    product_data: {
-                        name: product.name,
-                    },
-                    unit_amount: product.price * 100,
-                },
-                quantity: product.quantity,
-            },
-        ],
-        mode: 'payment',
-        success_url: "http://localhost:300/success",
-        cancel_url: "http://localhost:300/cancel"
-    });
-    res.json({id: session.id});
-})
+app.post('/api/pay/create', postStripePayment)
 
 //listen server
 app.listen(8900, () => {
