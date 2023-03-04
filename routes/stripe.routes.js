18022c464e278ed82063a079e54b878dbc75efc1
ItVal <valentinhonvj1@gmail.com>
2023-03-04 18:06:39 +0100

    feat create stripe routes

---
+++ routes/stripe.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const route = express.Router();
+const { postStripePayment } = require('../controllers/stripe.controllers');
+
+route.post('/', postStripePayment);
+
+export default route;
+
