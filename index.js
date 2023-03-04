21418a15acf479e623316456ecd2656e9ed670fa
ItVal <valentinhonvj1@gmail.com>
2023-03-04 18:33:28 +0100

    refact index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import express from 'express';
-import postStripePayment from './routes/stripe.routes.js';
+import {postStripePayment} from './routes/index.js';
 const app = express();
 
 //Middlewares 
